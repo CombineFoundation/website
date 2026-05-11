53caab46400209f24a5696a7f631a639eef86804
Combine Foundation <foundationcombine@gmail.com>
2026-05-11 19:51:42 +0500

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
-import type { Metadata } from "next";
-import { Montserrat, Inter } from "next/font/google";
+
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
@@ -8,21 +8,11 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { usePathname } from "next/navigation";
 
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
-
 const montserrat = Montserrat({
-  variable: "--font-montserrat",
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+  weight: ["400", "600", "700"],
+  variable: "--font-montserrat",
 });
-
-export const metadata: Metadata = {
-  title: "Combine Foundation",
-  description: "Combine Foundation is a non-profit organization dedicated to supporting and empowering individuals and communities through various initiatives and programs.",
-};
 
 export default function RootLayout({
   children,
@@ -35,10 +25,14 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${montserrat.variable} ${inter.variable} h-full antialiased`}
+      className={`${montserrat.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col font-(--font-montserrat)">
-        {children}
+      <body className="min-h-full flex flex-col font-[var(--font-montserrat)]">
+        {!isAdminPage && <Header />}
+        <main className="flex-grow flex flex-col">
+          {children}
+        </main>
+        {!isAdminPage && <Footer />}
         <SpeedInsights />
         <Analytics />
       </body>
